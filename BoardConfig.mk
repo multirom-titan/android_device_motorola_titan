TeamWin/android_device_motorola_victara

Compress recovery ramdisk using lzma
Change-Id: I5dea56289dc60448e1f0ed701c2e8893938462b6

BoardConfig.mk
@@ -19,6 +19,8 @@ TARGET_KRAIT_BIONIC_PLDSIZE := 64
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := victara
 
+BOARD_CUSTOM_BOOTIMG_MK := device/motorola/victara/mkbootimg.mk
+
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags
 
 BOARD_KERNEL_BASE := 0x0000000

multirom-titan/android_device_motorola_titan

BoardConfig.mk
@@ -2,6 +2,8 @@
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := MSM8226
 
+BOARD_CUSTOM_BOOTIMG_MK := device/motorola/titan/mkbootimg.mk
+
 # Platform
 TARGET_BOARD_PLATFORM := msm8226
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno305